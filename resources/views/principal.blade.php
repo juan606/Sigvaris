63e7d2e272fd983f3f42c2b5ca8c18e9c29304aa
Alan Ramirez <alanicio98@outlook.com>
2019-04-25 11:37:46 -0500

    Modificado url de la imagenheader del nav

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -19,7 +19,7 @@
 <body>
     <div class="row p-0 m-0">
         <div class="col-12 p-0 m-0">
-            <a href="/inicio">
+            <a href="{{ url('/inicio') }}">
                 <img id="imagenHeaderid" src="{{asset('img/header.jpg')}}" class="img-fluid" alt="Sigvaris.">
             </a>
         </div>
