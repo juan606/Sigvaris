488e246d842f3b9a92853c14599bd072d0fa0762
Cristian <cristianguzmansuarez@gmail.com>
2019-12-03 11:46:23 -0600

    Solucionando error con fecha de reporte 0

--- resources/views/reportes/diez.blade.php
+++ resources/views/reportes/diez.blade.php
@@ -84,17 +84,17 @@
                                     </th>
                                     <th>{{
                                         $doctor
-                                            ->pacientes()
-                                            ->withCount("ventas")
-                                            ->with('ventas')
-                                            // SOLO PACIENTES CON VENTAS EN EL RANGO DE TIEMPO
-                                            ->whereHas('ventas', function (\Illuminate\Database\Eloquent\Builder $query) use ($mes) {
-                                                $query->where('fecha', '>=', '2019-'.$mes.'-1')
-                                                    ->where('fecha', '<=', '2019-'.$mes.'-31');
-                                            })
-                                            // SOLO PACIENTES CON MAS DE UNA VENTA
-                                            ->having('ventas_count', '>=', 2)
-                                            ->get()->count()
+                                                ->pacientes()
+                                                ->withCount("ventas")
+                                                // SOLO PACIENTES CON VENTAS EN EL RANGO DE TIEMPO
+                                                ->whereHas('ventas', function(\Illuminate\Database\Eloquent\Builder $query) use($mes){
+                                                    $query->where('fecha','>=','2019-'.$mes.'-01')
+                                                        ->where('fecha', '<=', '2019-'.$mes.'-31');
+                                                })
+                                                // SOLO PACIENTES CON MENOS DE UNA VENTA
+                                                ->having('ventas_count', '>', 1)
+                                                ->get()
+                                                ->count()
                                         }}</th>
                                 @endforeach
                                 <td>-</td>
