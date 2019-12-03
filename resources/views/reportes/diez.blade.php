fdf182f25d8d9b7af9d4b6ca287d4b89f7bc217a
Cristian <cristianguzmansuarez@gmail.com>
2019-12-03 11:53:43 -0600

    Reporte 10 finalizado

--- resources/views/reportes/diez.blade.php
+++ resources/views/reportes/diez.blade.php
@@ -97,8 +97,36 @@
                                                 ->count()
                                         }}</th>
                                 @endforeach
-                                <td>-</td>
-                                <td>-</td>
+                                <td>
+                                    {{
+                                        $doctor
+                                            ->pacientes()
+                                            ->withCount("ventas")
+                                            // SOLO PACIENTES CON VENTAS EN EL RANGO DE TIEMPO
+                                            ->whereHas('ventas', function(\Illuminate\Database\Eloquent\Builder $query) use($mesesSolicitados){
+                                                $query->where('fecha','>=','2019-'.$mesesSolicitados[0].'-01')
+                                                    ->where('fecha', '<=', '2019-'.end($mesesSolicitados).'-31');
+                                            })
+                                            // SOLO PACIENTES CON MENOS DE UNA VENTA
+                                            ->having('ventas_count', '<=', 1)
+                                            ->get()
+                                            ->count()
+                                    }}
+                                </td>
+                                <td>{{
+                                        $doctor
+                                            ->pacientes()
+                                            ->withCount("ventas")
+                                            // SOLO PACIENTES CON VENTAS EN EL RANGO DE TIEMPO
+                                            ->whereHas('ventas', function(\Illuminate\Database\Eloquent\Builder $query) use($mesesSolicitados){
+                                                $query->where('fecha','>=','2019-'.$mesesSolicitados[0].'-01')
+                                                    ->where('fecha', '<=', '2019-'.end($mesesSolicitados).'-31');
+                                            })
+                                            // SOLO PACIENTES CON MENOS DE UNA VENTA
+                                            ->having('ventas_count', '>', 1)
+                                            ->get()
+                                            ->count()
+                                    }}</td>
                             </tr>
                         @endforeach
                     </tbody>    
