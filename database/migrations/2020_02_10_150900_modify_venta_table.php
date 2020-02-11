436d64ddb24efae86cff86bd2e22136c1f9586a4
Juan Nolasco <jjuanccarloss_96@hotmail.com>
2020-02-11 12:05:40 -0600

    Cambio migraciones

--- database/migrations/2020_02_10_150900_modify_venta_table.php
+++ database/migrations/2020_02_10_150900_modify_venta_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyVentasTable extends Migration
+class ModifyVentasTablePago extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2020_02_11_170929_modify_venta_table.php
+++ database/migrations/2020_02_11_170929_modify_venta_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyVentasTable extends Migration
+class ModifyVentasTableMeses extends Migration
 {
     /**
      * Run the migrations.
