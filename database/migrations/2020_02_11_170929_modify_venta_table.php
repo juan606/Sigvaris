9fda2d06d6342ff5ab71362db022015be7a89b9b
Juan Nolasco <jjuanccarloss_96@hotmail.com>
2020-02-11 12:14:32 -0600

    Cambio migraciones

--- database/migrations/2020_02_11_170929_modify_venta_table.php
+++ database/migrations/2020_02_11_170929_modify_venta_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyVentasTableMeses extends Migration
+class ModifyVentaTable extends Migration
 {
     /**
      * Run the migrations.
